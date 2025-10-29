0590314953def64cbfcd41489f3940433b8ade93
hotyoolmoocha <marcusyool2@naver.com>
2025-10-29 10:57:47 +0900

    ch06 진행중

--- Ch06/Assets/2. Scripts/PlayerController.cs
+++ Ch06/Assets/2. Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb;
+    Animator animator;
     public float JumpForce = 600f;
     public float walkForce = 30f;
     public float maxWalkSpeed = 2f;
@@ -14,6 +15,7 @@ public class PlayerController : MonoBehaviour
     {
         Application.targetFrameRate = 60;
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -39,6 +41,8 @@ public class PlayerController : MonoBehaviour
         {
             transform.localScale = new Vector3(key, 1, 1);
         }
+
+        animator.speed = speedX/2;
     }
 
 }
