da772192611ee673e71b2553c3f5db2d9042c233
hotyoolmoocha <marcusyool2@naver.com>
2025-11-03 17:48:58 +0900

    ch06 끝

--- Ch06/Assets/2. Scripts/PlayerController.cs
+++ Ch06/Assets/2. Scripts/PlayerController.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb;
     Animator animator;
-    public float JumpForce = 600f;
+    public float JumpForce = 500f;
     public float walkForce = 30f;
     public float maxWalkSpeed = 2f;
 
@@ -21,10 +23,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-
+        if (Input.GetKeyDown(KeyCode.Space)&&
+            rb.velocity.y == 0){ 
+      
             rb.AddForce(transform.up * JumpForce);
+            animator.SetTrigger("Jump Trigger");
         }
 
         int key = 0;
@@ -42,7 +45,24 @@ public class PlayerController : MonoBehaviour
             transform.localScale = new Vector3(key, 1, 1);
         }
 
+        if (rb.velocity.y == 0)
+        {
+            animator.speed = speedX / 2.0f;
+        }
+
         animator.speed = speedX/2;
+
+        if(transform.position.y< -10)
+        {
+            string sceneName = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene("sceneName");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Debug.Log("Goal");
+        SceneManager.LoadScene("Clear Scene");
     }
 
 }
