2ae344d66c6ade230ea4330f1127f72ada2a588b
hotyoolmoocha <marcusyool2@naver.com>
2025-11-05 10:20:37 +0900

    CH 06 종료
    끝

--- Ch06/Assets/2. Scripts/PlayerController.cs
+++ Ch06/Assets/2. Scripts/PlayerController.cs
@@ -65,4 +65,11 @@ public class PlayerController : MonoBehaviour
         SceneManager.LoadScene("Clear Scene");
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "cloud")
+        {
+            transform.parent = null;
+        }
+    }
 }
