08c2631a47722fe2ada6fbf50619306c607a730b
hotyoolmoocha <marcusyool2@naver.com>
2025-11-17 17:46:42 +0900

    ch07 끝 ch08 시작
    그렇대요

--- Ch07/Assets/2. Scripts/BamsongiController.cs
+++ Ch07/Assets/2. Scripts/BamsongiController.cs
@@ -4,26 +4,28 @@ using UnityEngine;
 
 public class BamsongiController : MonoBehaviour
 {
-    public int forwardForce = 2000;
-    public int upForce = 200;
+    public void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
 
+
+    void OnCollisionEnter(Collision other)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<ParticleSystem>().Play();
+    }
     // Start is called before the first frame update
     void Start()
     {
         Application.targetFrameRate = 60;
-        Shoot(new Vector3(0,200,2000));
+        //Shoot(new Vector3(0,200,2000));
         Destroy(gameObject, 10);
     }
 
     // Update is called once per frame
-    void Update(Vector3 dir)
-    {
-        GetComponent<Rigidbody>().AddForce(dir);
-        
-    }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        GetComponent<Rigidbody>()
-    }
+
+
+  
 }
